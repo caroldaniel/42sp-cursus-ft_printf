597107089c61fe818ca849624369f1d9fc582807
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-18 23:59:51 -0300

    Work in Progress

--- bonus/srcs/ft_parse.c
+++ bonus/srcs/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 16:38:09 by cado-car          #+#    #+#             */
-/*   Updated: 2021/08/18 17:51:31 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/18 23:59:45 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,6 +126,8 @@ void	ft_parse_precision(t_format *fmt, t_holder *h)
 void	ft_parse_conversion(t_format *fmt, t_holder *h)
 {
 	if (ft_strchr(HOLDER_CONVERSION, fmt->format[fmt->i]))
+	{
 		h->conversion = fmt->format[fmt->i];
-	fmt->i++;
+		fmt->i++;	
+	}
 }
