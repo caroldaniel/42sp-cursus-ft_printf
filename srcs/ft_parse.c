86f61f3e650001ae74e7d174a4ee9e6b4abaceb1
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-20 12:47:33 -0300

    Last touches

--- srcs/ft_parse.c
+++ srcs/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 16:38:09 by cado-car          #+#    #+#             */
-/*   Updated: 2021/08/20 12:46:04 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/20 12:47:25 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,7 +118,7 @@ void	ft_parse_precision(t_format *fmt, t_holder *h)
 void	ft_parse_conversion(t_format *fmt, t_holder *h)
 {
 	if (!ft_strchr(HOLDER_ALL, fmt->format[fmt->i]) \
-		ft_isprint(fmt->format[fmt->i]))
+		&& ft_isprint(fmt->format[fmt->i]))
 	{
 		h->conversion = fmt->format[fmt->i];
 		fmt->i++;
